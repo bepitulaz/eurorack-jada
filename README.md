e76a281d89438341b4a3f191827510da98600459
Asep Bagja Priandana <bepitulaz@gmail.com>
2024-04-18 22:16:49 +0300

    Building wifi captive portal

---
+++ README.md
@@ -0,0 +1,5 @@
+# Nanas Sound - Jada
+
+Jada means sequence in Estonian. It is a 2 CV and 2 gate sequencer for Eurorack. Jada can be sequenced over the Internet. It is a part of the Nanas Sound project.
+
+This repository still in the early stages of development.
---
+++ main/main.c
@@ -0,0 +1,23 @@
+#include "esp_wifi.h"
+#include "esp_log.h"
+#include "wifi_manager.h"
+
+/* @brief tag used for ESP serial console messages */
+static const char TAG[] = "main";
+
+void cb_connection_ok(void *pvParameter){
+	ip_event_got_ip_t* param = (ip_event_got_ip_t*)pvParameter;
+
+	/* transform IP to human readable string */
+	char str_ip[16];
+	esp_ip4addr_ntoa(&param->ip_info.ip, str_ip, IP4ADDR_STRLEN_MAX);
+
+	ESP_LOGI(TAG, "I have a connection and my IP is %s!", str_ip);
+}
+
+void app_main(void)
+{
+  wifi_manager_start();
+
+  wifi_manager_set_callback(WM_EVENT_STA_GOT_IP, &cb_connection_ok);
+}
