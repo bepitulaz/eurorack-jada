b2c7919b86eff293c24ea8efc82cdc689abb6acf
Asep Bagja Priandana <bepitulaz@gmail.com>
2024-04-22 22:37:56 +0300

    Changing the duty cycle and gate based on step data

--- main/step_sequencer.c
+++ main/step_sequencer.c
@@ -2,47 +2,30 @@
 #include <stdio.h>
 #include "step_sequencer.h"
 #include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/semphr.h"
+#include "driver/gpio.h"
 #include "driver/ledc.h"
 #include "esp_err.h"
 
-#define LEDC_TEST_DUTY (4000)
-#define LEDC_TEST_FADE_TIME (3000)
-
-/*
- * This callback function will be called when fade operation has ended
- * Use callback only if you are aware it is being called inside an ISR
- * Otherwise, you can use a semaphore to unblock tasks
- */
-static IRAM_ATTR bool cb_ledc_fade_end_event(const ledc_cb_param_t *param, void *user_arg)
-{
-  BaseType_t taskAwoken = pdFALSE;
-
-  if (param->event == LEDC_FADE_END_EVT)
-  {
-    SemaphoreHandle_t counting_sem = (SemaphoreHandle_t)user_arg;
-    xSemaphoreGiveFromISR(counting_sem, &taskAwoken);
-  }
-
-  return (taskAwoken == pdTRUE);
-}
+#define LEDC_CV_1_DUTY (4096) // Set duty to 100%. (2 ** 12) * 100%
+#define GATE_1_GPIO (10)
+#define CV_1_GPIO (23)
 
 void step_sequencer_init()
 {
-  // Initialize dummy step data. It's an array of StepData structs.
-  // Create a dummy sequence with 16 steps and random number of pitch, modulation (freq), and gate values (1 and 0).
-  StepData dummy_sequence[16];
+  // Initialize step data. It's an array of StepData structs.
+  // Create a sequence with 16 steps and random number of pitch, modulation (freq), and gate values (1 and 0).
+  StepData sequence[16];
+  float frequency_range_min = 65.41; // C2
+  float frequency_range_max = 523.25; // C5
   for (int i = 0; i < 16; i++)
   {
-    dummy_sequence[i].step = i;
-    dummy_sequence[i].pitch = 440.0 + (rand() % 1000) / 100.0;
-    dummy_sequence[i].gate = rand() % 2;
+    sequence[i].pitch = frequency_range_min + ((float)rand() / RAND_MAX) * (frequency_range_max - frequency_range_min);
+    sequence[i].gate = rand() % 2;
   }
 
   ledc_timer_config_t ledc_timer = {
-      .duty_resolution = LEDC_TIMER_13_BIT,
-      .freq_hz = 4000,
+      .duty_resolution = LEDC_TIMER_12_BIT,
+      .freq_hz = 5000,
       .speed_mode = LEDC_LOW_SPEED_MODE,
       .timer_num = LEDC_TIMER_0,
       .clk_cfg = LEDC_AUTO_CLK,
@@ -50,56 +33,46 @@ void step_sequencer_init()
 
   ledc_timer_config(&ledc_timer);
 
-  ledc_channel_config_t ledc_channel_1 = {
+  ledc_channel_config_t ledc_cv_channel_1 = {
       .channel = LEDC_CHANNEL_0,
-      .duty = 0,
-      .gpio_num = 15,
+      .duty = LEDC_CV_1_DUTY,
+      .gpio_num = CV_1_GPIO,
       .speed_mode = LEDC_LOW_SPEED_MODE,
       .timer_sel = LEDC_TIMER_0,
       .hpoint = 0,
       .flags.output_invert = 0,
   };
 
-  ledc_channel_config(&ledc_channel_1);
+  ledc_channel_config(&ledc_cv_channel_1);
 
-  ledc_fade_func_install(0);
+  gpio_config_t gate_conf_channel_1 = {
+      .intr_type = GPIO_INTR_DISABLE,
+      .mode = GPIO_MODE_OUTPUT,
+      .pin_bit_mask = (1ULL << GATE_1_GPIO),
+      .pull_down_en = 0,
+      .pull_up_en = 0,
+  };
 
-  ledc_cbs_t callbacks = {
-      .fade_cb = cb_ledc_fade_end_event};
+  gpio_config(&gate_conf_channel_1);
 
-  SemaphoreHandle_t counting_sem = xSemaphoreCreateCounting(1, 0);
+  int tempo_bpm = 50; // It should be set by the user.
+  int delay_time = 60000 / tempo_bpm; // Calculate delay time for each beat
 
-  ledc_cb_register(ledc_channel_1.speed_mode, ledc_channel_1.channel, &callbacks, (void *)counting_sem);
-
+  int sequence_length = sizeof(sequence) / sizeof(sequence[0]);
+  int step = 0;
   while (1)
   {
-    printf("1. LEDC fade up to duty = %d\n", LEDC_TEST_DUTY);
+    // Get the current step data, and make sure it will go back to the first step after the last step.
+    StepData current_step = sequence[step % sequence_length];
 
-    ledc_set_fade_with_time(ledc_channel_1.speed_mode,
-                            ledc_channel_1.channel, LEDC_TEST_DUTY, LEDC_TEST_FADE_TIME);
-    ledc_fade_start(ledc_channel_1.speed_mode,
-                    ledc_channel_1.channel, LEDC_FADE_NO_WAIT);
+    printf("1. Gate channel 1 set %d\n", current_step.gate);
+    gpio_set_level(GATE_1_GPIO, current_step.gate);
 
-    xSemaphoreTake(counting_sem, portMAX_DELAY);
+    printf("2. CV channel 1 set to %f\n", current_step.pitch);
+    ledc_set_freq(LEDC_LOW_SPEED_MODE, LEDC_TIMER_0, current_step.pitch);
 
-    printf("2. LEDC fade down to duty = 0\n");
-    ledc_set_fade_with_time(ledc_channel_1.speed_mode,
-                            ledc_channel_1.channel, 0, LEDC_TEST_FADE_TIME);
-    ledc_fade_start(ledc_channel_1.speed_mode,
-                    ledc_channel_1.channel, LEDC_FADE_NO_WAIT);
+    vTaskDelay(delay_time / portTICK_PERIOD_MS);
 
-    xSemaphoreTake(counting_sem, portMAX_DELAY);
-
-    printf("3. LEDC set duty = %d without fade\n", LEDC_TEST_DUTY);
-    ledc_set_duty(ledc_channel_1.speed_mode, ledc_channel_1.channel, LEDC_TEST_DUTY);
-    ledc_update_duty(ledc_channel_1.speed_mode, ledc_channel_1.channel);
-
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
-
-    printf("4. LEDC set duty = 0 without fade\n");
-    ledc_set_duty(ledc_channel_1.speed_mode, ledc_channel_1.channel, 0);
-    ledc_update_duty(ledc_channel_1.speed_mode, ledc_channel_1.channel);
-
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    step++;
   }
 }
